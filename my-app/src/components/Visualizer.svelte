506264682a31e6c042a7548d1851694d32f198bb
philipsoitu <95496892+philipsoitu@users.noreply.github.com>
2024-03-30 19:41:14 -0400

    beginning shdl visualization

---
+++ my-app/src/components/Visualizer.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let width = 500;
+	export let height = 500;
+	export let shdl = '';
+
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D | null = null;
+
+	onMount(() => {
+		ctx = canvas.getContext('2d');
+
+		draw();
+	});
+
+	function draw() {
+		if (ctx === null) {
+			console.error('Failed to get 2D context for canvas.');
+		} else {
+			// Set line width
+			ctx.lineWidth = 10;
+
+			// Wall
+			ctx.strokeRect(75, 140, 150, 110);
+
+			// Door
+			ctx.fillRect(130, 190, 40, 60);
+
+			// Roof
+			ctx.beginPath();
+			ctx.moveTo(50, 140);
+			ctx.lineTo(150, 60);
+			ctx.lineTo(250, 140);
+			ctx.closePath();
+			ctx.stroke();
+
+			ctx.font = '30px Arial';
+			ctx.fillText(shdl, 10, 50);
+		}
+	}
+</script>
+
+<canvas {width} {height} bind:this={canvas} style="border:1px solid #000000;" />
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,8 +1,9 @@
-<main>
-	<h1>SvelteKit & Supabase Auth</h1>
-	<p>Let's learn how to register and login users!</p>
-	<div class="auth-buttons">
-		<a href="/login" class="btn btn-primary">Login</a>
-		<a href="/register" class="btn btn-secondary">Register</a>
-	</div>
-</main>
+<script>
+	import Visualizer from '../components/Visualizer.svelte';
+	let text = '';
+	$: shdl = text;
+</script>
+
+<div class="mx-5 my-5">
+	<Visualizer width={500} height={500} {shdl} />
+</div>
