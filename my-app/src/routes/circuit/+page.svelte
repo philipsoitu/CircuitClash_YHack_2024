24cf84e0f6af0b9b8b498c13a5c48aab745e86df
Gabriel Mikus <113815444+mikusgabriel@users.noreply.github.com>
2024-03-31 09:37:23 -0400

    types

--- my-app/src/routes/circuit/+page.svelte
+++ my-app/src/routes/circuit/+page.svelte
@@ -16,12 +16,19 @@
 
 	export let data;
 
+	let tb: any;
+	let ioData: any;
+	let inputData: any;
+	let outputData: any;
+
 	// Assuming data.messages[0].io contains structured data you might need to separate or process further
-	console.log(data.messages[0].io);
-	let ioData = JSON.parse(data.messages[0].io.replace(/'/g, '"')); // Ensure this is correctly structured for parsing
-	let inputData = ioData.input; // Hypothetical structure
-	let outputData = ioData.output; // Hypothetical structure
-	let tb = JSON.parse(data.messages[0].expectedoutput.replace(/'/g, '"')); // Ensure this is correctly structured for parsing
+	onMount(() => {
+		console.log(data.messages[0].io);
+		ioData = JSON.parse(data.messages[0].io.replace(/'/g, '"')); // Ensure this is correctly structured for parsing
+		inputData = ioData.input; // Hypothetical structure
+		outputData = ioData.output; // Hypothetical structure
+		tb = JSON.parse(data.messages[0].expectedoutput.replace(/'/g, '"')); // Ensure this is correctly structured for parsing
+	});
 
 	console.log(inputData);
 	console.log(outputData);
@@ -55,9 +62,35 @@
 		}
 	}
 
+	async function sendText(text: string) {
+		try {
+			let response = await fetch('/api/text', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'text/html'
+				},
+				body: text // Modify this string as needed
+			});
+
+			if (response.ok) {
+				const responseData = await response.json();
+				console.log(response);
+				if (responseData['answer'] == 'true') {
+					let lvl: number = parseInt(data.messages[0].id);
+					lvl++;
+					updateLevel(lvl);
+				}
+			} else {
+				console.error('Failed to process string');
+			}
+		} catch (error) {
+			console.error('Error processing string:', error);
+		}
+	}
+
 	async function updateLevel(level: number) {
 		try {
-			let response = await fetch('/api/message', {
+			let response = await fetch('/api/level', {
 				method: 'POST',
 				body: JSON.stringify(level.toString()) // Modify this string as needed
 			});
@@ -79,6 +112,7 @@
 			buttontextindex = 0;
 		}
 	}
+	let text = `Write your answer here..`;
 </script>
 
 <main class="container flex flex-1 flex-col">
@@ -114,14 +148,15 @@
 					theme={oneDark}
 					placeholder={'Start coding here..'}
 				/>
-				<div>
+				<div class="flex flex-col-2 mx-auto gap-4 mt-4">
+					<button class="btn" on:click={changeText}>{buttontext[buttontextindex]}</button>
+
 					<button
 						class="btn"
 						on:click={() =>
 							sendCode(JSON.stringify({ code: value, number: parseInt(data.messages[0].id) }))}
 						>Submit</button
 					>
-					<button class="btn mt-4" on:click={changeText}>{buttontext[buttontextindex]}</button>
 				</div>
 			</div>
 		</div>
@@ -131,7 +166,24 @@
 				<div class="card">
 					<h1 class="h1">Q{data.messages[0].id}: {data.messages[0].name}</h1>
 					<p class="questiondesc">{data.messages[0].description}</p>
-					<TruthTable truthTable={tb} {inputData} {outputData} />
+				</div>
+
+				<div class="col-span-1 my-4 hidden rounded-xl bg-primary p-4 lg:grid overflow-y-auto">
+					<textarea
+						class="card width: 100%
+							maxWidth: 100%
+							height: 25em"
+						bind:value={text}
+					/>
+
+					<div class=" flex flex-col-1 mx-auto gap-4 mt-4">
+						<button
+							class="btn"
+							on:click={() =>
+								sendCode(JSON.stringify({ code: value, number: parseInt(data.messages[0].id) }))}
+							>Submit</button
+						>
+					</div>
 				</div>
 			</div>
 
@@ -142,6 +194,26 @@
 				</div>
 			</div>
 		</div>
-		<button class="btn mt-4" on:click={changeText}>{buttontext[buttontextindex]}</button>
-	{:else}{/if}
+	{:else}
+		<div class="grid flex-1 gap-2 lg:grid-cols-2">
+			<div class="flex h-full flex-col gap-4 overflow-y-auto p-4">
+				<div class="card">
+					<h1 class="h1">Q{data.messages[0].id}: {data.messages[0].name}</h1>
+					<p class="questiondesc">{data.messages[0].description}</p>
+				</div>
+
+				<div class="col-span-1 my-4 hidden rounded-xl bg-primary p-4 lg:grid overflow-y-auto">
+					
+
+				</div>
+			</div>
+
+			<div class="flex h-full flex-col gap-4 overflow-y-auto p-4">
+				<div class="card">
+					<h1 class="h1">Circuit:</h1>
+					<Visualizer width={700} height={400} shdl={value} />
+				</div>
+			</div>
+		</div>
+	{/if}
 </main>
